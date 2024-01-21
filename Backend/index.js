2685c72aa2c05c3a055f89a0699aeb9e8d4e01a3
hamza1851 <hamza.dev011@gmail.com>
2024-01-21 14:50:54 +0530

    Setup frontend

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,21 @@ import { PORT, mongoDBURL } from "./config.js"
 import mongoose from "mongoose"
 import { Book } from "./models/bookModel.js"
 import booksRoute from "./routes/booksRoutes.js"
+import cors from "cors"
 
 const app = express()
 
 //Adding Middleware for parsing request body
 app.use(express.json())
+
+//middleware for cors policy
+// app.use(
+//   cors({
+//     origin: "http//localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Cotent-Type"],
+//   })
+// )
 
 app.get("/", (req, res) => {
   console.log(req)
